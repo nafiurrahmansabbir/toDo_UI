5c5a804331fb2ef0f1a2e612d5d6f108aeaf4522
nafiurrahmansabbir <nafiurrahmansabbir@gmail.com>
2024-06-24 11:43:22 +0600

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ToDo extends StatelessWidget {
+  const ToDo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:todo_ui/app.dart';
+
+void main(){
+  runApp(const ToDo());
+}
